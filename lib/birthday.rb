e4e2d5975bb8ec914dd347cce03da4f03aacd891
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-17 04:31:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,10 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    if birthday_kids: ,12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
   end
 end
 
