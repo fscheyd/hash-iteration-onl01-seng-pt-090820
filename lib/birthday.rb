4ed77ff7347ec0988508d60379cd136d27090aac
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-17 04:47:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,8 +11,10 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  if age < 12
-    puts "You are too old for this."
+  birthday_kids.each do |kids_name,age|
+    if age > [12]
+      puts "You are too old for this."
+    end
   end
 end
 
